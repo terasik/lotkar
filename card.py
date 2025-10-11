41abf8070100f57cc8c2efb52af1fba3166e7076
terasik trubadur <artemgn@yahoo.com>
2025-10-11 08:30:20 +0200

    cards sound implemented

--- card.py
+++ card.py
@@ -2,9 +2,11 @@
 
 import logging
 import random
+from glob import glob
 import arcade
 from pyglet.graphics import Batch
 from defs import *
+
 
 class Card(arcade.Sprite):
     """
@@ -25,11 +27,12 @@ class Card(arcade.Sprite):
         self.new_deck()
         position=(CELLS_FIELD_WIDTH-5-CARD_SIZE[0]/2,
                               GAME_STATUS_SPRITE_HEIGHT/2)
-
+        self.card_sounds=[]
         super().__init__(visible=False, center_x=position[0], center_y=position[1])
 
     def setup(self):
         self.textures=[arcade.load_texture(f"resources/card_{i+1}.png") for i in range(CARD_TYPE_CNT)]
+        self.load_card_sounds()
 
     def update(self, delta_time):
         self.timer_change+=delta_time
@@ -43,6 +46,7 @@ class Card(arcade.Sprite):
                 self.timer_end=0
                 deck_nr=random.randrange(self.deck_len)
                 self.card_nr=self.deck[deck_nr]
+                self.play_card_sound()
                 self.set_texture(self.card_nr)
                 del self.deck[deck_nr]
                 self.deck_len-=1
@@ -54,6 +58,25 @@ class Card(arcade.Sprite):
 
     def _update(self):
         pass
+
+    def load_card_sounds(self):
+        logging.info("lade karten sounds")
+        for n in range(1,CARD_TYPE_CNT+1):
+            sound_list=[]
+            fsl=glob(f"resources/card_{n}_*.[wm][pa][3v]")
+            for s in fsl:
+                sound_list.append(arcade.Sound(s))
+            self.card_sounds.append(sound_list)
+        logging.info("gefundene sounds: %s", self.card_sounds)
+
+
+    def play_card_sound(self):
+        sound_list=self.card_sounds[self.card_nr]
+        if sound_list:
+            s=random.choice(sound_list)
+            logging.info("spiele karten sound %s ab", s.file_name) 
+            s.play()
+
 
     def new_deck(self):
         logging.info("neues kartenstapel wird erzeugt")
@@ -71,6 +94,7 @@ class Card(arcade.Sprite):
             else:
                 raise RuntimeError("sth wrong with mr. random")
         self.deck_len=sum(CARD_CNT)
+        logging.info("neues kartenstapel: %s", self.deck)
         #print(f"scc: {scc}")
         #print(f"new deck of cards: {self.deck} {len(self.deck)}")
 
